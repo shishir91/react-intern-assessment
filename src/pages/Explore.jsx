b8b17be8199db1d4db2989f5b8afe6d21a53d5c6
Shishir Shrestha <shishirshrestha@ismt.edu.np>
2024-03-03 23:40:19 +0545

    exp

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -18,24 +18,125 @@ const Explore = () => {
           cursor: "pointer",
         }}
       >
-        <img
-          src={product.image}
-          style={{
-            height: "250px",
-            width: "250px",
-            objectFit: "contain",
-          }}
-        />
+        <div
+          id="default-carousel"
+          className="relative w-full"
+          data-carousel="slide"
+        >
+          <div className="relative h-50 overflow-hidden rounded-lg md:h-92">
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src="https://cdn.dummyjson.com/product-images/30/thumbnail.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+
+              {/* {product.images.map((image, index) => {
+                return (
+                  <>
+                    <div key={index}>
+                      <img
+                        src={image}
+                        className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        alt="..."
+                      />
+                    </div>
+                  </>
+                );
+              })} */}
+            </div>
+          </div>
+          <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+            <button
+              type="button"
+              className="w-3 h-3 rounded-full"
+              aria-current="true"
+              aria-label="Slide 1"
+              data-carousel-slide-to="0"
+            ></button>
+            <button
+              type="button"
+              className="w-3 h-3 rounded-full"
+              aria-current="false"
+              aria-label="Slide 2"
+              data-carousel-slide-to="1"
+            ></button>
+            <button
+              type="button"
+              className="w-3 h-3 rounded-full"
+              aria-current="false"
+              aria-label="Slide 3"
+              data-carousel-slide-to="2"
+            ></button>
+            <button
+              type="button"
+              className="w-3 h-3 rounded-full"
+              aria-current="false"
+              aria-label="Slide 4"
+              data-carousel-slide-to="3"
+            ></button>
+            <button
+              type="button"
+              className="w-3 h-3 rounded-full"
+              aria-current="false"
+              aria-label="Slide 5"
+              data-carousel-slide-to="4"
+            ></button>
+          </div>
+          <button
+            type="button"
+            className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-prev
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg
+                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+              <span className="sr-only">Previous</span>
+            </span>
+          </button>
+          <button
+            type="button"
+            className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-next
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg
+                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span className="sr-only">Next</span>
+            </span>
+          </button>
+        </div>
         <br />
-        <h3>{product.name}</h3>
-        Author: <br />
-        <small>{product.author}</small>
+        <h3 className="font-bold text-2xl font-semibold tracking-widest rounded-lg">{product.title}</h3>
+         
+        <span><b>Brand</b>: {product.brand}</span>
+        <span><b>Price: </b>${product.price}</span>
+        <span><b>Discount: </b>{product.discountPercentage}%</span>
+        <span><b>Rating: </b>{product.rating}/5</span>
+        <span><b>Description: </b>{product.description}</span>
         <br />
-        Genre: <br />
-        <small>{product.genre}</small>
-        <br />
-        Description: <br />
-        <small>{product.description}</small>
       </div>
     </div>
   );
